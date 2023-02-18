c4f036a22453baba0e8ed3f1eeb728ce440d7b28
Mtab <107020397+DamenaM@users.noreply.github.com>
2023-02-18 07:04:41 +0300

    adding API

--- src/Modules/display.js
+++ src/Modules/display.js
@@ -1,16 +1,12 @@
-import { getData } from './api.js';
-
-const leaderboard = document.querySelector('.display');
-// - display the scores.
-const displayScores = async () => {
-  const scores = await getData();
-  const scoresObj = scores.result;
-  scoresObj.forEach((score) => {
-    const scoreContainer = document.createElement('div');
-    scoreContainer.classList.add('score_list_container');
-    scoreContainer.innerHTML = `${score.name}: ${score.score}`;
-    leaderboard.appendChild(scoreContainer);
-  });
+const tableBody = document.querySelector('.tableBody');
+const displayScores = (arr) => {
+  tableBody.innerHTML = '';
+  const displayScores = arr.map(
+    (item) => `
+          <tr >
+          <td class="score-container" ><span>${item.user} : ${item.score}</span></td>
+        </tr>`,
+  );
+  tableBody.innerHTML = displayScores.join(' ');
 };
-
 export default displayScores;
